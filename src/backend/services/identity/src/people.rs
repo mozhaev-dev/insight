b5f5d6bd935e04a9aaa7288c5935e89546bf3121
Anton Zelenov <antonz@constructor.tech>
2026-05-04 10:02:44 +0200

    Do not allow to discover available URLs for non auth users

--- src/backend/services/identity/src/people.rs
+++ src/backend/services/identity/src/people.rs
@@ -196,7 +196,7 @@ impl PeopleStore {
         let mut aliases = HashMap::new();
         aliases.insert(
             "test@vz.com".to_owned(),
-            "oleksii.shponarskyi@virtuozzo.com".to_owned(),
+            "some@person-at-vz.com".to_owned(),
         );
 
         Self { by_email, aliases }
